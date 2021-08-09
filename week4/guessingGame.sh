f2edd4e7697c309aa8aef30dffdf50ca7392e169
Ahmed Naseem <ahmed.nacm@gmail.com>
2021-08-09 13:47:47 +0000

    Week 4 scripts added to portfolio

---
+++ week4/guessingGame.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#4.3
+
+#Ask to neter a number between 1 and 100.
+#read -p "Enter a number between 1 and 100:" userGuess
+
+#getNumber() 
+getNumber()
+{ 
+    read -p "$1: " 
+    while (( $REPLY != 42 )); do 
+        #Check if entered value is between 1 and 100.
+        if (( $REPLY < 1 ||  $REPLY > 100 )); then
+                echo -e "\033[31mERROR:\033[0m Input must be between 1 and 100"    
+        elif (( $REPLY < 42 )); then
+            echo "Too low!"
+            else 
+                 echo "Too high!"
+        fi 
+        read -p "$1: " 
+    done 
+    #when user enters corret number -42
+    echo "Correct!"
+} 
+
+#Call the function getNumber()
+getNumber "Enter a number between 1 and 100: " 
