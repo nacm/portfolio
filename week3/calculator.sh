dac599f03549fe5db416416a9be55ef4f50ef600
Ahmed Naseem <ahmed.nacm@gmail.com>
2021-08-12 13:45:15 +0000

    Updated calculator with colors

--- week3/calculator.sh
+++ week3/calculator.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #3.2 Calculator
+#color codes all operations
+_GREEN=$(tput setaf 2)
+_BLUE=$(tput setaf 4)
+_RED=$(tput setaf 1)
+_PURPLE=$(tput setaf 5)
+_RESET=$(tput sgr0)
+_BOLD=$(tput bold)
 
 #Take user inputs
 
@@ -12,25 +19,41 @@ PS3='Please Enter calculation option: '
 
 #Valid options
 options=(
-    Addition
-    Subtraction
-    Multiplication
-    Division
+    ${_BLUE}Addition${_RESET}
+    ${_GREEN}Subtraction${_RESET}
+    ${_RED}Multiplication${_RESET}
+    ${_PURPLE}Division${_RESET}
 )
 select  opt in "${options[@]}"
 do  
     case $REPLY in
     1)
     #Addition
+    res=`echo $a + $b | bc`
     ;;
     2)
     ##Subtraction
+    res=`echo $a - $b | bc`
     ;;
     3)
     #Multiplication
+    res=`echo $a \* $b | bc`
     ;;
     4)
-    #Division
+    #Division with 2 decimals
+    res=`echo "scale=2; $a / $b" | bc`
     ;;
+
     esac
+    #Display Answer
+    echo "Result: ${_BOLD}$res${_RESET}"
+
+     # exit by entering q
+    echo "Press 'Q' to exit or any key to continue"
+    #Check what user enters next
+    read nextstep
+    if [ "$nextstep" = "q" ] || [ "$nextstep" = "Q" ]; then
+        break
+    fi
+
 done
